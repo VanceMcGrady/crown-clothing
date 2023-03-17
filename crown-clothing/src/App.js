7adb2a8f6e989a2e6f3543bd05e3c2892df468ad
Vance M Mcgrady <68658084+VanceMcGrady@users.noreply.github.com>
2023-03-17 16:01:58 -0700

    added scss

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
 
 const  App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    }, 
+    {
+      id: 4,
+      title: 'Womens'
+    }, 
+    {
+      id: 5,
+      title: 'Mens'
+    },      
+
+  ]
+
   return (
+  
     <div className="categories-container">
-      <div className='category-container'>
+      {categories.map((category) => {
+        return (
+        <div className='category-container'>
+         <div className='background-image'></div>
+         <div className='category-body-container'>
+          <h2>{category.title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+        )
+      })}
+      {/* <div className='category-container'>
         <img src='blank' alt='blank'/>
         <div className='category-body-container'>
           <h2>Hats</h2>
@@ -37,7 +74,7 @@ const  App = () => {
           <h2>Mens</h2>
           <p>Shop Now</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
